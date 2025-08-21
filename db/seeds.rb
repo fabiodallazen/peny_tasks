4d6b96b039449b23b57637705adcf37ce8b923b3
Fabio Andre Dallazen <fabiodallazen@gmail.com>
2025-08-21 15:15:03 -0300

    Add development seeds to generate multiple sample tasks with Faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,20 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# frozen_string_literal: true
+
+require 'faker'
+
+return unless Rails.env.development?
+
+puts "Seeding tasks..."
+Task.destroy_all
+
+statuses = %w[pending completed]
+
+20.times do
+  Task.create!(
+    title: Faker::Lorem.sentence(word_count: 3),
+    description: Faker::Lorem.paragraph(sentence_count: 2),
+    status: statuses.sample
+  )
+end
+
+puts "Created #{Task.count} tasks!"
